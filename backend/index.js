e5c46e6cc5ca44d35636f08fd5eeefd0719deb78
mahesh <banagarmahesh2002@gmail.com>
2022-02-07 12:49:47 +0530

    added router

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const connectToMongoose = require('./db')
+const express = require('express')
+
+connectToMongoose()
+
+
+const app = express()
+const port = 3000
+app.use(express.json())
+//available routes
+// app.use("/api/notes",require('./routes/notes'))
+app.use("/api/auth",require('./routes/auth'))
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
