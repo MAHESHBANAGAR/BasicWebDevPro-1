a08435f693d87ab4f77a371c26aa327726a41420
mahesh <banagarmahesh2002@gmail.com>
2022-02-07 16:11:36 +0530

    added jsonwebtoken

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 //create user using:POST /api/auth/createuser
 
@@ -16,18 +18,37 @@ router.post('/createuser',[
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    let user = await User.findOne({email:req.body.email});
-    if(user){
-        return res.status(400).json({error:"sry a user with this email already exists"})
+    
+    try {
+        let user = await User.findOne({email:req.body.email});
+        if(user){
+            return res.status(400).json({error:"sry a user with this email already exists"})
+        }
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password,salt);
+        //creates a new user
+        user= await User.create({
+            name: req.body.name,
+            password: secPass,
+            email: req.body.email,
+        })
+        const data = {
+            user:{
+                id:user.id,
+            }
+        }
+
+        const authtoken = jwt.sign(data,"mysecretkey");
+        console.log(authtoken)
+        // res.json({authtoken})
+        res.json({user})
+
+        
+    } catch (error) {
+        //if there is error returns message
+        console.error(error.message);
+        res.status(500).send('something error has occured');
     }
-    user= await User.create({
-        name: req.body.name,
-        password: req.body.password,
-        email: req.body.email,
-    })
-    // .then(user => res.json(user))
-    // .catch(err => res.status(400).json(err))
-    res.json({"nice":"you have created a user"});
 })
 
 module.exports = router
